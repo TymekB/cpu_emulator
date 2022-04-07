92e0ab6686fc981c1d81915897ef0a8af802980a
unknown <Tymoteusz.Baran@microsoft.wsei.edu.pl>
2022-04-07 14:37:35 +0200

    init

---
+++ js/main.js
@@ -0,0 +1,20 @@
+let app = new Vue({
+    el: '#app',
+    data: {
+        register: [
+            'f963',
+            'c5f0',
+            '9e46',
+            '35b1',
+        ]
+    },
+    methods: {
+        test: function (start, end) {
+            
+        },
+        
+    },
+    created() {
+        console.log("hello world");
+    }
+});
