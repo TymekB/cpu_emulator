c518a5dffe07b7351cee3660e25d756e59ce0d8c
TymekB <baran.tymoteusz@gmail.com>
2022-04-11 12:49:14 +0200

    Add getRegisterIndexByName method

--- js/main.js
+++ js/main.js
@@ -11,13 +11,20 @@ new Vue({
         to: "",
     },
     methods: {
+        getRegisterIndexByName: function(name) {
+            return this.register.findIndex((el) => el.name === name);
+        },
         move: function (from, to) {
+            const fromIndex = this.getRegisterIndexByName(from);
+            const toIndex = this.getRegisterIndexByName(to);
             const fromIndex = this.register.findIndex((el) => el.name === from);
             const toIndex = this.register.findIndex((el) => el.name === to);
 
             this.register[toIndex].value = this.register[fromIndex].value;
         },
         exchange: function (from, to) {
+            const fromIndex = this.getRegisterIndexByName(from);
+            const toIndex = this.getRegisterIndexByName(to);
             const fromIndex = this.register.findIndex((el) => el.name === from);
             const toIndex = this.register.findIndex((el) => el.name === to);
             const temp = this.register[fromIndex].value;
