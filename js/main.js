ed1c8a3960b25ff9ac76e0ee781b53f6af72c12f
TymekB <baran.tymoteusz@gmail.com>
2022-04-12 20:21:44 +0200

    Add preventing from moving or exchanging the same registers

--- js/main.js
+++ js/main.js
@@ -14,19 +14,18 @@ new Vue({
         getRegisterIndexByName: function(name) {
             return this.register.findIndex((el) => el.name === name);
         },
+        isDisabled: function() {
+            return this.from === this.to && this.from !== "" && this.to !== "";
+        },
         move: function (from, to) {
             const fromIndex = this.getRegisterIndexByName(from);
             const toIndex = this.getRegisterIndexByName(to);
-            const fromIndex = this.register.findIndex((el) => el.name === from);
-            const toIndex = this.register.findIndex((el) => el.name === to);
 
             this.register[toIndex].value = this.register[fromIndex].value;
         },
         exchange: function (from, to) {
             const fromIndex = this.getRegisterIndexByName(from);
             const toIndex = this.getRegisterIndexByName(to);
-            const fromIndex = this.register.findIndex((el) => el.name === from);
-            const toIndex = this.register.findIndex((el) => el.name === to);
             const temp = this.register[fromIndex].value;
 
             this.register[fromIndex].value = this.register[toIndex].value;
