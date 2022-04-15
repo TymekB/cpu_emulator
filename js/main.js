001a4d1d4da6dd9dc5a713e2d5c8b8850468c7ac
TymekB <baran.tymoteusz@gmail.com>
2022-04-15 16:17:59 +0200

    Add validation to move and exchange methods

--- js/main.js
+++ js/main.js
@@ -15,15 +15,19 @@ new Vue({
             return this.register.findIndex((el) => el.name === name);
         },
         isDisabled: function() {
-            return this.from === this.to && this.from === "" || this.to === "";
+            return this.from === this.to || this.from === "" || this.to === "";
         },
         move: function (from, to) {
+            if(this.isDisabled()) return;
+
             const fromIndex = this.getRegisterIndexByName(from);
             const toIndex = this.getRegisterIndexByName(to);
 
             this.register[toIndex].value = this.register[fromIndex].value;
         },
         exchange: function (from, to) {
+            if(this.isDisabled()) return;
+
             const fromIndex = this.getRegisterIndexByName(from);
             const toIndex = this.getRegisterIndexByName(to);
             const temp = this.register[fromIndex].value;
@@ -31,8 +35,5 @@ new Vue({
             this.register[fromIndex].value = this.register[toIndex].value;
             this.register[toIndex].value = temp;
         },
-    },
-    created() {
-
     }
 });
